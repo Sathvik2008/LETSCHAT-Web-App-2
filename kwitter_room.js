15d6e79c77c902a52f0ff6deca727bfded59b847
Sathvik2008 <109099815+Sathvik2008@users.noreply.github.com>
2023-05-09 19:45:23 -0400

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,10 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyADR5O8Raf3LxKanVTci3x62Ax6mDKPzFE",
+  authDomain: "letschat-web-app-6df27.firebaseapp.com",
+  projectId: "letschat-web-app-6df27",
+  storageBucket: "letschat-web-app-6df27.appspot.com",
+  messagingSenderId: "1042349757261",
+  appId: "1:1042349757261:web:ce12012d9d5c7473599d04"
+};
+const app = initializeApp(firebaseConfig);
